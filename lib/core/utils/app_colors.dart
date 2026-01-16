647083a5fd9e5bfe8da2eb05b19f906a4198792f
hazem-web <hazemlotfy74@gmail.com>
2026-01-16 17:48:02 +0200

    start

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFF03AA77);
+  static const Color greyColor = Color(0xFF8789A3);
+  static const Color titleColor = Color(0xFF0F543F);
+  static const Color lightGreenColor = Color(0xFF43B692);
+  static const Color boldGreenColor = Color(0xFF099773);
+}
---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyle {
+  static const TextStyle semiBold16 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const TextStyle semiBold18 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  );
+  static const TextStyle regular13 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+  );
+  static const TextStyle medium15 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 15,
+  );
+  static const TextStyle medium14 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+  static const TextStyle bold64 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 64,
+  );
+  static const TextStyle bold15 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 15,
+  );
+  static const TextStyle bold20 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  );
+  static const TextStyle regular20 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+  );
+  static const TextStyle semiBold12 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+  );
+}
